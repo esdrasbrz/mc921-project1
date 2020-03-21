55508d1372427c56950313aa9beeb5b06a00821c
luiz <luiz.e.a.zucchi@gmail.com>
2020-03-21 14:26:43 -0300

    - Add unnittest
    - token id
    - int const
    - string const
    - ucomment
    
    Signed-off-by: luiz <luiz.e.a.zucchi@gmail.com>

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -19,11 +19,45 @@ def test_lexer(lex):
 def test_ccomment(lex):
     lex.input('/* test block comment */')
 
-    while True:
-        tok = lex.token()
-        assert not tok
+    tok = lex.token()
+    assert not tok
 
-        if not tok:
-            break  # No more input
-    pass
+
+def test_ucomment(lex):
+    lex.input('// test inline comment')
+
+    tok = lex.token()
+    assert not tok
+
+
+def test_id(lex):
+    lex.input('_int')
+    tok = lex.token()
+    assert str(tok) == "LexToken(ID,'_int',8,0)"
+
+    lex.input('_float1')
+    tok = lex.token()
+    assert str(tok) == "LexToken(ID,'_float1',8,0)"
+
+    lex.input('string')
+    tok = lex.token()
+    assert str(tok) == "LexToken(ID,'string',8,0)"
+
+
+def test_newline(lex):
+    lex.input('\n')
+    tok = lex.token()
+    assert not tok
+
+
+def test_int_const(lex):
+    lex.input('3412')
+    tok = lex.token()
+    assert str(tok) == 'LexToken(INT_CONST,3412,9,0)'
+
+
+def test_string_const(lex):
+    lex.input('"Hello World"')
+    tok = lex.token()
+    assert str(tok) == 'LexToken(STRING_CONST,\'"Hello World"\',9,0)'
 
