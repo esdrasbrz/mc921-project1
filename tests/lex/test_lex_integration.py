fff5300e7c8db89e5379a212d61c7a131bb867b0
Luiz Zucchi <luizeduardoaraujozucchi@gmail.com>
2020-04-04 18:14:23 -0300

    Remove unused imports Remove deprecated tests
    Signed-off-by: luiz <luiz.e.a.zucchi@gmail.com>

--- tests/lex/test_lex_integration.py
+++ tests/lex/test_lex_integration.py
@@ -1,9 +1,3 @@
-import pytest
-
-from tests.conftest import read_files
-from .data import *
-
-
 def test_lex_identifiers(lex, tests):
     """
     Integration test for the lex, tests identifiers
@@ -20,45 +14,4 @@ def test_lex_identifiers(lex, tests):
     output = output.rstrip('\n')
     assert output == tests[1]
 
-    pass
-
-
-def test_lex_reserved(lex):
-    """
-    Integration test for the lex, tests reserved keywords
-    :return:
-    """
-    lex.input(lex_reserved_test)
-
-    while True:
-        tok = lex.token()
-        if not tok:
-            break  # No more input
-    pass
-
-
-def test_lex_all(lex):
-    """
-    Integration test for the lexer, test all possible cases
-    :return
-    """
-    lex.input(lex_test_all)
-
-    while True:
-        tok = lex.token()
-        if not tok:
-            break  # No more input
-    pass
-
-
-def test_example_6(lex):
-    """
-    Integration test for the lexer, test all possible cases
-    :return:
-    """
-    lex.input(lex_example_6)
-    while True:
-        tok = lex.token()
-        if not tok:
-            break  # No more input
     pass
