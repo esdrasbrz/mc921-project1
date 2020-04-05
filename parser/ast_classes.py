48f8949445481dd76935c205f2a8475d3de3df09
Luiz Zucchi <luizeduardoaraujozucchi@gmail.com>
2020-04-05 15:00:56 -0300

    - Add binary operation class
    Signed-off-by: luiz <luiz.e.a.zucchi@gmail.com>

--- parser/ast_classes.py
+++ parser/ast_classes.py
@@ -182,6 +182,31 @@ class UnaryOp(Node):
 
     attr_names = ('op', )
 
+class BinaryOp(Node):
+    __slots__ = ('op', 'left', 'right', 'coord')
+    def __init__(self, op, left, right, coord=None):
+        self.op = op
+        self.left = left
+        self.right = right
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        if self.left is not None:
+            nodelist.append(("left", self.left))
+        if self.right is not None:
+            nodelist.append(("right", self.right))
+        return tuple(nodelist)
+
+    def __iter__(self):
+        if self.left is not None:
+            yield self.left
+        if self.right is not None:
+            yield self.right
+
+    attr_names = ('op', )
+
+
 class ID(Node):
     __slots__ = ('name', 'coord')
     def __init__(self, name, coord=None):
